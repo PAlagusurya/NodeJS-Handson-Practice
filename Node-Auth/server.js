1869a2f74604b9df2f559e8a3d6f2a51327d1672
Alagusurya P <alagusuryap@gmail.com>
2026-05-18 23:53:33 +0530

    Added required routes

--- Node-Auth/server.js
+++ Node-Auth/server.js
@@ -3,11 +3,19 @@ require("dotenv").config();
 const express = require("express");
 const connectToDB = require("./database/db");
 
+const authRoutes = require("./routes/auth");
+const adminRoutes = require("./routes/admin");
+const homeRoutes = require("./routes/home");
+
 const app = express();
 const PORT = process.env.PORT || 3001;
 
 // Middleware
 app.use(express.json());
+
+app.use("/api/auth", authRoutes);
+app.use("/api/admin", adminRoutes);
+app.use("/api/home", homeRoutes);
 
 // Connect to DB
 connectToDB();
